0f5cb611e8c4f377541e5b50e8e6d31bb29adbfa
Mosab Gaafow <devmuscab@gmail.com>
2024-09-15 18:03:59 +0300

    configured last time

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export const port = process.env.PORT;
 
 // Database url
-export const dbURL = process.env.DATABASE_URL;
+export const dbURL = process.env.DATABASE_URL; 
 // secret key
 export const JWT_Secret = process.env.JWT_SECRET_KEY;
 
